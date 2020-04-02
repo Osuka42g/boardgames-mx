e8478af2e991a99dc716dcaf3de3f3498b859f35
Oscar Mendoza <osuka42@gmail.com>
2020-04-01 22:18:33 -0600

    Dynamic urls

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,11 +1,23 @@
 import Layout from '../components/Layout';
+import Link from 'next/link';
 
-export default function Index() {
+const PostLink = props => (
+  <li>
+    <Link href={`/post?title=${props.title}`}>
+      <a>{props.title}</a>
+    </Link>
+  </li>
+);
+
+export default function Blog() {
   return (
-    <div>
-      <Layout>
-        <p>Hello Next.js</p>
-      </Layout>
-    </div>
+    <Layout>
+      <h1>My Blog</h1>
+      <ul>
+        <PostLink title="Hello Next.js" />
+        <PostLink title="Learn Next.js is awesome" />
+        <PostLink title="Deploy apps with Zeit" />
+      </ul>
+    </Layout>
   );
 }
