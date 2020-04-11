80b4ba7311ae50915d12ecb8d68df6ede8d5e08e
Oscar Mendoza <osuka42@gmail.com>
2020-04-10 22:41:09 -0500

    Add google analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,9 @@
+import Router from "next/router";
+import withGA from "next-ga";
 import 'semantic-ui-css/semantic.min.css'
 
-export default function App({ Component, pageProps }) {
+function App({ Component, pageProps }) {
   return <Component {...pageProps} />
 }
+
+export default withGA("UA-163337621-1", Router)(App);
